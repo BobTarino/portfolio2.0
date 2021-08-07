495c689bf976363cd6eb31ce306e8b39c0e0fece
Bobby T <bob.tarino@gmail.com>
2021-08-06 20:59:49 -0500

    used switch statement to conditionally render project categories

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,12 +1,32 @@
-import React, { useState } from 'react';
+import React, { } from 'react';
 
-const Project= ({ category }) => {
-    if(useState === currentCategory.name('Portfolio'))
-        return (
-            <section>
-             <h1>Color</h1>
-            </section>
-        );
+const Project= ({ currentCategory }) => {
+    
+    const conditionalRender = () => {
+        switch (currentCategory.name) {
+            case 'About': 
+
+                return <h1>About</h1>;
+            
+            case 'Portfolio':
+            
+                return <h1>Portfolio</h1>;
+
+            case 'Resume':
+
+                return <h1>Resume</h1>;
+
+            default:
+                return <h1>About</h1>;
+        }
+
+
+    }
+    return <h1>{conditionalRender()}</h1>;
+
+
+
+
 }
 
 
