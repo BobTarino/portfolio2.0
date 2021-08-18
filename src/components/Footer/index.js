011a4cf257af2754c6bf96a2b96a56f61ea21598
Bobby T <bob.tarino@gmail.com>
2021-08-17 23:47:34 -0500

    changed size of icons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,9 +7,9 @@ function Footer () {
         <section id="contact-info">
         <div class="list">
           <ul>
-            <li><a href="mailto: bob.tarino@gmail.com"><FiMail /></a></li>
-            <li><a href="https://github.com/BobTarino"><FaGithub /></a></li>
-            <li><a href="https://www.linkedin.com/in/roberttarino/"><FaLinkedin /></a></li>
+            <li><a href="mailto: bob.tarino@gmail.com"><FiMail size={40} /></a></li>
+            <li><a href="https://github.com/BobTarino"><FaGithub size={40} /></a></li>
+            <li><a href="https://www.linkedin.com/in/roberttarino/"><FaLinkedin size={40} /></a></li>
           </ul>
         </div>
     </section>
