6df3d3248aa17a50c0829175d09936485e06d46e
Bobby T <bob.tarino@gmail.com>
2021-08-17 23:31:01 -0500

    added react-icons for stylized footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { FiMail } from "react-icons/fi";
 
 function Footer () {
     return (
         <section id="contact-info">
         <div class="list">
           <ul>
-            <li><strong>Email:</strong> <a href="mailto: bob.tarino@gmail.com">bob.tarino@gmail.com</a></li>
-            <li><strong>Github:</strong> <a href="https://github.com/BobTarino">BobTarino</a></li>
-            <li><strong>LinkedIn:</strong> <a href="https://www.linkedin.com/in/roberttarino/">in/roberttarino</a></li>
+            <li><a href="mailto: bob.tarino@gmail.com"><FiMail /></a></li>
+            <li><a href="https://github.com/BobTarino"><FaGithub /></a></li>
+            <li><a href="https://www.linkedin.com/in/roberttarino/"><FaLinkedin /></a></li>
           </ul>
         </div>
     </section>
